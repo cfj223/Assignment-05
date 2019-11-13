08a4b5e56afc720c222f94bfec2bf54a2d6d9a14
Christyan Jean-Charles <cfj223@lehigh.edu>
2019-11-13 15:01:49 -0500

    Assignment 05

---
+++ Assignment 05.R
@@ -0,0 +1,7 @@
+#Assignemnt 05
+DF <- DEXUSEU
+if(!require("data.table")) install.packages("data.table")
+library("data.table")
+DF <- fread("DEXUSEU.csv", header = "auto",
+            data.table=FALSE)
+DF
